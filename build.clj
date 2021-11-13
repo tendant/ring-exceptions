f2b1600e6dfbc1188a541ca3c9471c66be14c089
Lei Wang <tendant@gmail.com>
2021-11-13 06:29:36 -0800

    add new build

---
+++ build.clj
@@ -0,0 +1,41 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]
+            [simple.build :as sb]))
+
+(def lib 'org.clojars.wang/ring-exceptions)
+
+;; if you want a version of MAJOR.MINOR.COMMITS:
+(def version (format "1.0.%s" (b/git-count-revs nil)))
+
+(defn jar
+  [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/clean)
+      (bb/jar)))
+
+(defn tag
+  [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (sb/tag)))
+
+(defn uberjar
+  [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/clean)
+      (sb/uberjar)))
+
+(defn install
+  [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (sb/install)))
+
+(defn release
+  [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (sb/release)))
